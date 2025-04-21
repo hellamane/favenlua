6e3e053f3300e2abda374513015b01d0f273a57a
hellamane <82798562+hellamane@users.noreply.github.com>
2025-04-21 19:40:53 -0400

    Update ORE.lua

--- ORE.lua
+++ ORE.lua
@@ -1,54 +1,79 @@
 --// Ore Mining Incremental | Discord steppin0nsteppas
 
-local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt"))()
+local Players = game:GetService("Players")
 
+local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt"))()
 local win = DiscordLib:Window("Ferret Hub V2")
 local serv = win:Server("Home", "")
-local lbls = serv:Channel("Info")
+local info = serv:Channel("Info")
 
-lbls:Label("By dawid#7205 & steppin0nsteppas")
-lbls:Seperator()
-lbls:Label("Welcome to Ferret Hub!")
-lbls:Seperator()
+info:Label("By dawid#7205 & steppin0nsteppas")
+info:Seperator()
+info:Label("Welcome to Ferret Hub!")
+info:Seperator()
 
-local woogy = serv:Channel("Autofarm")
+local getgood = serv:Channel("OP Features")
 
-woogy:Button("Get Stupidly OP", function()
-    local player = game:GetService("Players").LocalPlayer
-
-    local function setValue(object, propertyName, newValue)
-        if object and object:FindFirstChild(propertyName) and (object[propertyName]:IsA("IntValue") or object[propertyName]:IsA("NumberValue")) then
-            object[propertyName].Value = newValue
-            print("Set " .. propertyName .. " to " .. newValue)
+getgood:Textbox("WinLevel", "Enter Number", true, function(t)
+    local num = tonumber(t)
+    if num then
+        local p = Players.LocalPlayer
+        local winLevel = p:FindFirstChild("WinLevel")
+        if winLevel and winLevel:IsA("NumberValue") then
+            winLevel.Value = num
         else
-            warn("Failed to set " .. propertyName .. ".  Object or property not found, or not a NumberValue/IntValue. Object: " .. tostring(object) .. " Property: " .. propertyName)
+            winLevel = Instance.new("NumberValue")
+            winLevel.Name = "WinLevel"
+            winLevel.Value = num
+            winLevel.Parent = p
         end
     end
+end)
 
-    if player:FindFirstChild("leaderstats") and player.leaderstats:IsA("Folder") then
-        setValue(player.leaderstats, "Value", 9.2125e+87)
-    else
-        warn("leaderstats not found")
+getgood:Textbox("True Reset", "Enter Number", true, function(t)
+    local num = tonumber(t)
+    if num then
+        local p = Players.LocalPlayer
+        local trueReset = p:FindFirstChild("True Reset")
+        if trueReset and trueReset:IsA("NumberValue") then
+            trueReset.Value = num
+        else
+            trueReset = Instance.new("NumberValue")
+            trueReset.Name = "True Reset"
+            trueReset.Value = num
+            trueReset.Parent = p
+        end
     end
+end)
 
-    setValue(player, "WinLevel", 100000)
-
-    setValue(player, "True Reset", 500)
-
-    setValue(player, "Multiplier", 2302)
-
-    if player:FindFirstChild("Infinity") and (player.Infinity:IsA("IntValue") or player.Infinity:IsA("NumberValue")) then
-        player.Infinity.Value = 213000
-        print("Set Infinity to 213000")
-    else
-        warn("Infinity not found or incorrect type")
+getgood:Textbox("Multiplier", "Enter Number", true, function(t)
+    local num = tonumber(t)
+    if num then
+        local p = Players.LocalPlayer
+        local multiplier = p:FindFirstChild("Multiplier")
+        if multiplier and multiplier:IsA("NumberValue") then
+            multiplier.Value = num
+        else
+            multiplier = Instance.new("NumberValue")
+            multiplier.Name = "Multiplier"
+            multiplier.Value = num
+            multiplier.Parent = p
+        end
     end
+end)
 
-    local child8 = player:GetChildren()[8]
-    if child8 and (child8:IsA("IntValue") or child8:IsA("NumberValue")) then
-        child8.Value = 213000
-        print("Set 8th child (" .. child8.Name .. ") to 213000")
-    else
-        warn("8th child not found or incorrect type")
+getgood:Textbox("Infinity", "Enter Number", true, function(t)
+    local num = tonumber(t)
+    if num then
+        local p = Players.LocalPlayer
+        local infinity = p:FindFirstChild("Infinity")
+        if infinity and infinity:IsA("NumberValue") then
+            infinity.Value = num
+        else
+            infinity = Instance.new("NumberValue")
+            infinity.Name = "Infinity"
+            infinity.Value = num
+            infinity.Parent = p
+        end
     end
 end)
