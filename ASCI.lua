aa4596a4ebc4b98c73c37d3aab3f49db3a4c0597
hellamane <82798562+hellamane@users.noreply.github.com>
2026-08-06 21:32:05 -0400

    Load Discord library from remote URL

--- ASCI.lua
+++ ASCI.lua
@@ -3,7 +3,6 @@
 
 
 
-local DiscordLib =
 local DiscordLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt")()
 
 local win = DiscordLib:Window("faven.lua")
